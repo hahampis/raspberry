f804c67febc0667064ec05c37c968468dcf6b372
Nikolaos Chachampis <nikos_chachampis@ownmail.net>
2016-09-11 10:42:09 +0300

    Script for starting tuneinradio from the command line

---
+++ start_music.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+""" Fetch the list of recent stations from tuneinradio plugin """
+""" and start playing the first one """
+
+import subprocess
+import time
+from kodipydent import Kodi
+
+## Turn on the TV via CEC over HDMI 
+subprocess.call('/usr/bin/kodi-send --action="XBMC.CECActivateSource"', shell=True)
+
+my_kodi = Kodi('localhost', 80)
+my_kodi.Player.Stop(0)
+time.sleep(5)
+stations = my_kodi.Files.GetDirectory('plugin://plugin.audio.tuneinradio/?path=recents')
+station = stations['result']['files'][0]
+my_kodi.Player.Open.im_self.timeout = 120
+my_kodi.Player.Open(0, item={u'file': station['file']})
