d381a9956002bb81f08534ad695fead672648c93
Nikolaos Chachampis <nikos_chachampis@ownmail.net>
2016-09-12 20:20:31 +0300

    Start playing only if kodi is not already playing something.

--- start_music.py
+++ start_music.py
@@ -7,11 +7,20 @@ import subprocess
 import time
 from kodipydent import Kodi
 
-## Turn on the TV via CEC over HDMI 
-subprocess.call('/usr/bin/kodi-send --action="XBMC.CECActivateSource"', shell=True)
 
-my_kodi = Kodi('localhost', 80)
-stations = my_kodi.Files.GetDirectory('plugin://plugin.audio.tuneinradio/?path=recents')
-station = stations['result']['files'][0]
-my_kodi.Player.Open.im_self.timeout = 120
-my_kodi.Player.Open(0, item={u'file': station['file']})
+def kodi_is_playing():
+    if my_kodi.Player.GetActivePlayers()['result'] == []:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    my_kodi = Kodi('localhost', 80)
+    if not kodi_is_playing():
+        ## Turn on the TV via CEC over HDMI 
+        subprocess.call('/usr/bin/kodi-send --action="XBMC.CECActivateSource"', shell=True)
+        stations = my_kodi.Files.GetDirectory('plugin://plugin.audio.tuneinradio/?path=recents')
+        station = stations['result']['files'][0]
+        my_kodi.Player.Open.im_self.timeout = 120
+        my_kodi.Player.Open(0, item={u'file': station['file']})
